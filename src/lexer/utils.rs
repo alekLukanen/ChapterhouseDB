a9fd6f05c77078a9c8a78398b238e64cbae1d0eb
alekLukanen <aleklukanen@gmail.com>
2024-10-24 22:09:18 -0500

    defined the basic tokenization trait

--- src/lexer/utils.rs
+++
@@ -1,3 +0,0 @@
-pub fn lex_util() -> i32 {
-    52
-}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-use core::panic;
-use std::str::FromStr;
-
 use chapterhouseqe::lexer::builder;
 
 fn main() {
     println!("Hello, world!");
     let query = "
         select * from bike 
-        where id = 42 and value > 90.0";
+        where id = 42 and value > 90.0 and name = '🥵'";
 
-    let query_val = match String::from_str(query) {
-        Ok(v) => v,
-        Err(_) => panic!("invalid string"),
-    };
-
-    let tokens = builder::lex(query_val);
+    let tokens = builder::lex(query);
 
     println!("tokens from lexer: {:?}", tokens);
 }
