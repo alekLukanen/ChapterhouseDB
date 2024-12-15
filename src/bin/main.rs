96b7264fc42df1a5b676273789f0d7b9620d9186
alekLukanen <aleklukanen@gmail.com>
2024-12-15 13:59:52 -0600

    add command line arguments to help with testing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,31 @@
-use chapterhouseqe::worker::{QueryWorker, QueryWorkerConfig};
-use tracing::info;
+use clap;
 use tracing_subscriber;
 
+use chapterhouseqe::worker::{QueryWorker, QueryWorkerConfig};
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The port used to accept incoming connections
+    #[arg(short, long, default_value_t = 7000)]
+    port: u32,
+
+    // Addresses to connect to
+    #[arg(short, long, default_value = "127.0.0.1:7001")]
+    connect_to_addresses: Vec<String>,
+}
+
 fn main() {
+    let args = Args::parse();
+
     tracing_subscriber::fmt::init();
 
-    info!("creating the worker");
-
     let mut worker = QueryWorker::new(QueryWorkerConfig::new(
-        "127.0.0.1:7000".to_string(),
-        vec!["127.0.0.1:7001".to_string()],
+        format!("127.0.0.1:{}", args.port),
+        args.connect_to_addresses,
     ));
 
     match worker.start() {
