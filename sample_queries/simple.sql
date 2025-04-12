67c7b38b09609656e32cdcaebc707a98793bf4eb
alekLukanen <aleklukanen@gmail.com>
2025-04-12 17:28:45 -0500

    add better example computations

--- sample_queries/simple.sql
+++ sample_queries/simple.sql
@@ -15,7 +15,10 @@ select id, value2 from read_files('simple/*.parquet')
 select 
   id, 
   id + 10.0 as id_plus_10, 
-  (value2 + 10) / 100 as value2 
+  (value2 + 10) / 100 as value2,
+  1.0 / id as value3,
+  1.0 / (id * id) as value4,
+  id * id as value5
 from read_files('simple/*.parquet')
   where id > 25 + 0.0;
 
